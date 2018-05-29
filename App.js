89b8f9cdc436abf92f38ce028c134280fcc9c643
keevn <keevn@163.com>
2018-05-29 03:50:42 -0400

    add navigator

--- App.js
+++ App.js
@@ -4,55 +4,88 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import TabNavigator from 'react-native-tab-navigator';
+
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image,
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+export default class emoChat extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.unReadMassage = '';
+        this.defaultState = 'chat';
+        this.state = {
+            selectedTab: this.defaultState,
+            unReadMassage: '',
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TabNavigator>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'chat'}
+                        title="Chat"
+                        renderIcon={() => <Image style={[styles.icon, {tintColor: '#808080'}]}
+                                                 source={require('./res/images/ic_chat.png')}/>}
+                        renderSelectedIcon={() => <Image style={styles.icon}
+                                                         source={require('./res/images/ic_chat.png')}/>}
+                        badgeText={this.unReadMassage}
+                        onPress={() => {
+                            this.unReadMassage = '';
+                            this.setState({selectedTab: 'chat'});
+                        }}>
+                        <View style={styles.page1}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'setting'}
+                        title="Setting"
+                        renderIcon={() => <Image style={[styles.icon, {tintColor: '#808080'}]}
+                                                 source={require('./res/images/ic_settings.png')}/>}
+                        renderSelectedIcon={() => <Image style={styles.icon}
+                                                         source={require('./res/images/ic_settings.png')}/>}
+                        onPress={() => {
+                            this.unReadMassage = '2';
+                            this.setState({selectedTab: 'setting'});
+                        }}>
+                        <View style={styles.page2}></View>
+                    </TabNavigator.Item>
+                </TabNavigator>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-      textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    page1: {
+        flex: 1,
+        backgroundColor: '#F5FCcc',
+    },
+    page2: {
+        flex: 1,
+        backgroundColor: '#F5FCaa',
+    },
+    navigatorItem: {
+        padding: 5,
+    },
+    icon: {}
 });
