b8865ec6e710217a3de198db41aa99a4c50c3750
keevn <keevn@163.com>
2018-05-29 01:46:02 -0400

    change background color

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+      backgroundColor: '#F5FCaa',
   },
   welcome: {
     fontSize: 20,
